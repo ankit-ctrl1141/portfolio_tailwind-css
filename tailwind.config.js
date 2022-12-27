943bf6de387b1fb2be24b6b02dc144cc5e6f1c55
Ankit Kumar Verma <ankit.official025@gmail.com>
2022-12-27 18:28:31 +0530

    navbar_hamburger_menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "sm" : "480px"  //break point for small screen devices.
+        // here it means {min-width:480px to above all}
+      }
+    },
+    fontFamily:{
+      nunito : ['Nunito', 'sans-serif']
+    }
   },
   plugins: [],
 }
